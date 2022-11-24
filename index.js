0efa35de76294a17ad6aca7ec0f99cdf4746b649
Jeff <jcgilbert70@gmail.com>
2022-11-24 11:54:23 -0600

    working on init and writeToFile functions, first test of questions working

--- index.js
+++ index.js
@@ -1,45 +1,47 @@
 // TODO: Include packages needed for this application
-const addInquirer = require('inquirer');
-const addFs = require('fs');
+const inquirer = require('inquirer');
+const fs = require('fs');
 const addMarkdown = require('./utils/generateMarkdown');
 
 
 // TODO: Create an array of questions for user input
 const questions = [
-// enter user name
+
+    // enter user name
     {
-    type: 'input',
-    name: 'username',
-    message: 'Enter your Github Username'
-},
-// enter email
-{
-type: 'input',
-name: 'email',
-message: 'Enter your Email address'
-},
+        type: 'input',
+        name: 'username',
+        message: 'Enter your Github Username: '
+    },
 
-// enter project name
-{
-type: 'input',
-name: 'projectName',
-message: 'Enter your Project name'
-},
+    // enter email
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your Email address: '
+    },
 
-// enter description
-{
-type: 'input',
-name: 'description',
-message: 'Enter a description of your project'
-},
+    // enter project name
+    {
+        type: 'input',
+        name: 'projectName',
+        message: 'Enter your Project name: '
+    },
 
-// select a liscense from a list
-{
-type: 'list',
-name: 'liscense',
-message: 'Select a liscense for your project',
-options: ['Apache', 'Academic', 'GNU', 'ISC', 'MIT', 'Moilla', 'Open', '']
-},
+    // enter description
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description of your project: '
+    },
+
+    // select a liscense from a list
+    {
+        type: 'list',
+        name: 'liscense',
+        message: 'Select a liscense for your project:',
+        options: ['Apache', 'Academic', 'GNU', 'ISC', 'MIT']
+    },
 
 ];
 
@@ -55,10 +57,24 @@ options: ['Apache', 'Academic', 'GNU', 'ISC', 'MIT', 'Moilla', 'Open', '']
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        err ? console.log(err) : console.log("created");
+    });
+  }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((data) => {
+            let dataObject = data;
+
+            console.log(dataObject);
+
+            writeToFile(dataObject);
+        })
+}
 
 // Function call to initialize app
 init();
